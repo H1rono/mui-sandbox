53881b4ae135f0c41dfe5c34f31d7cc4ea7f914f
H1rono <hronok66@gmail.com>
2022-10-18 00:26:07 +0900

    ✨ Add Counter, robot fonts

---
+++ src/Counter.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <Box>
+            <Typography variant="h2">Counter</Typography>
+            <Typography>Count is: {count}</Typography>
+            <Button variant="outlined" onClick={() => setCount(count + 1)}>
+                +
+            </Button>
+            <Button variant="outlined" onClick={() => setCount(count - 1)}>
+                -
+            </Button>
+            <Button variant="outlined" onClick={() => setCount(0)}>
+                reset
+            </Button>
+        </Box>
+    );
+};
+
+export default Counter;
